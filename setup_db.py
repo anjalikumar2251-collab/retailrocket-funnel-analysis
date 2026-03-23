ef690609210c3c1f0479398a5b804ed80d6d566e
anjalikumar2251-collab <anjalikumar2251@gmail.com>
2026-03-23 13:27:04 +0530

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -1,11 +1,13 @@
+import os
 import pandas as pd
 import sqlite3
 
 print("Loading CSV...")
-df = pd.read_csv(r"C:\Users\Anjali S\Desktop\Kaggle RetailRocket\events.csv")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+df = pd.read_csv(os.path.join(BASE_DIR, "events.csv"))
 
 print("Creating database...")
-conn = sqlite3.connect('retailrocket.db')
+conn = sqlite3.connect(os.path.join(BASE_DIR, 'retailrocket.db'))
 
 print("Writing to database...")
 df.to_sql('events', conn, if_exists='replace', index=False)
